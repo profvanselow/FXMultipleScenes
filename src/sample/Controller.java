16bba0eefbc3d7b3521423f6a30e257d14d6a2ca
Prof. Vanselow <profvanselow@gmail.com>
2018-09-08 14:37:35 -0400

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+
+    @FXML
+    private void button1Pressed(ActionEvent event) throws IOException {
+        // System.out.println("in button1pressed");
+
+        Stage stage = Main.getPrimaryStage();
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample2.fxml"));
+
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
+
+    }
+
+    @FXML
+    private void button2Pressed(ActionEvent event) throws IOException {
+        //System.out.println("in button2pressed");
+
+        Stage stage = Main.getPrimaryStage();
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        setPrimaryStage(primaryStage);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+
+        primaryStage.show();
+    }
+
+    private void setPrimaryStage(Stage stage) {
+        Main.primaryStage = stage;
+    }
+
+    static public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
